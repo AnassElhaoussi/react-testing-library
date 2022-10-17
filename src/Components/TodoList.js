226ed2a347e891e5fcc95eab9b16042fb76f20cc
anasselhaoussi <anous.elhaoussi@gmail.com>
2022-10-17 23:36:03 +0200

    feat : started working on the todo list app

---
+++ src/Components/TodoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const TodoList = () => {
+    return <div className='todo-list'>
+        <div className='header'>
+            <h1 className='heading-1'>What's Up!</h1>
+            <h3>Here are your tasks</h3>
+        </div>
+    </div>
+        
+}
